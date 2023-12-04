38bfba062b72b12c80386131dbfe6b6ea1f03c42
AndroPyro28 <menandroeugenio1028@gmail.com>
2023-12-04 08:57:05 +0800

    user patient hash password

--- MediWise/database/mediwise.sql
+++ MediWise/database/mediwise.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.1
+-- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 29, 2023 at 03:15 PM
--- Server version: 10.4.28-MariaDB
--- PHP Version: 8.2.4
+-- Generation Time: Dec 04, 2023 at 01:52 AM
+-- Server version: 10.4.22-MariaDB
+-- PHP Version: 8.1.1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -29,22 +29,22 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `admin` (
   `admin_id` int(11) NOT NULL,
-  `username` varchar(250) NOT NULL,
-  `password` varchar(250) NOT NULL,
-  `first_name` varchar(250) DEFAULT NULL,
-  `middle_name` varchar(250) DEFAULT NULL,
-  `last_name` varchar(250) DEFAULT NULL,
-  `email_address` varchar(250) DEFAULT NULL,
-  `contact_number` varchar(250) DEFAULT NULL,
-  `status` varchar(100) NOT NULL DEFAULT 'ACTIVE'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `username` varchar(250) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `password` varchar(250) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `first_name` varchar(250) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `middle_name` varchar(250) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `last_name` varchar(250) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `email_address` varchar(250) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `contact_number` varchar(250) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `status` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'ACTIVE'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Dumping data for table `admin`
 --
 
 INSERT INTO `admin` (`admin_id`, `username`, `password`, `first_name`, `middle_name`, `last_name`, `email_address`, `contact_number`, `status`) VALUES
-(1, 'admin', '123', 'admin', NULL, NULL, NULL, NULL, 'ACTIVE');
+(1, 'admin123', '123', 'john', 'rizz', 'doe', 'johndoe@gmail.com', '09561289642', 'ACTIVE');
 
 -- --------------------------------------------------------
 
@@ -54,18 +54,23 @@ INSERT INTO `admin` (`admin_id`, `username`, `password`, `first_name`, `middle_n
 
 CREATE TABLE `appointment` (
   `appointment_id` int(11) NOT NULL,
-  `description` varchar(255) NOT NULL,
-  `doctor_id` varchar(255) NOT NULL,
-  `date` date NOT NULL,
-  `status` varchar(255) NOT NULL DEFAULT 'PENDING'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `description` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `doctor_id` int(11) DEFAULT NULL,
+  `patient_id` int(11) DEFAULT NULL,
+  `date` datetime(3) NOT NULL,
+  `status` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'PENDING',
+  `request_status` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'PENDING',
+  `doctor_name` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Dumping data for table `appointment`
 --
 
-INSERT INTO `appointment` (`appointment_id`, `description`, `doctor_id`, `date`, `status`) VALUES
-(4, 'test', '4', '2023-11-25', 'PENDING');
+INSERT INTO `appointment` (`appointment_id`, `description`, `doctor_id`, `patient_id`, `date`, `status`, `request_status`, `doctor_name`) VALUES
+(7, 'sikret', 1, NULL, '2023-12-04 13:33:10.653', 'PENDING', 'ACCEPTED', 'Andro'),
+(8, '', NULL, NULL, '2023-12-04 04:32:10.724', 'PENDING', 'DECLINE', 'andro eugenio'),
+(9, 'asdad', NULL, NULL, '2023-12-05 04:32:20.254', 'PENDING', 'ACCEPT', 'asdad');
 
 -- --------------------------------------------------------
 
@@ -75,23 +80,25 @@ INSERT INTO `appointment` (`appointment_id`, `description`, `doctor_id`, `date`,
 
 CREATE TABLE `doctor` (
   `doctor_id` int(11) NOT NULL,
-  `first_name` varchar(255) NOT NULL,
-  `middle_name` varchar(255) DEFAULT NULL,
-  `last_name` varchar(255) NOT NULL,
-  `suffix` varchar(255) NOT NULL,
-  `specialist` varchar(255) NOT NULL,
-  `id_number` varchar(255) NOT NULL,
-  `barangay` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `first_name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `middle_name` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `last_name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `suffix` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `specialist` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `id_number` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `barangay` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `password` varchar(250) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `username` varchar(250) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `status` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'ACTIVE'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Dumping data for table `doctor`
 --
 
-INSERT INTO `doctor` (`doctor_id`, `first_name`, `middle_name`, `last_name`, `suffix`, `specialist`, `id_number`, `barangay`) VALUES
-(3, 'testt', 'mid', 'last', '', 'sample', 'DOCTOR10012', ''),
-(4, 'doctor2', 'doctor2', 'doctor2', 'jr', 'doctor2', 'doctor2', '178 A'),
-(5, 'fsflkjlkj', 'lkj', 'lkj', 'lkj', 'lkj', '12312', '');
+INSERT INTO `doctor` (`doctor_id`, `first_name`, `middle_name`, `last_name`, `suffix`, `specialist`, `id_number`, `barangay`, `password`, `username`, `status`) VALUES
+(1, 'vic', 'bren', 'magnaye', '', 'Prenatal Care Practitioner', '123121', '143 brgy', '123', 'doctor123', 'ACTIVE'),
+(2, 'Menandro', 'Talla', 'Eugenio', 'jr', 'secret', '13213123', '175', '', '', 'ACTIVE');
 
 -- --------------------------------------------------------
 
@@ -101,11 +108,36 @@ INSERT INTO `doctor` (`doctor_id`, `first_name`, `middle_name`, `last_name`, `su
 
 CREATE TABLE `event` (
   `event_id` int(11) NOT NULL,
-  `title` text NOT NULL,
-  `description` text NOT NULL,
-  `image` longtext NOT NULL,
-  `date` date NOT NULL DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `title` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `description` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `image` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `date` datetime(3) NOT NULL DEFAULT current_timestamp(3)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `inventory`
+--
+
+CREATE TABLE `inventory` (
+  `inventory_id` int(11) NOT NULL,
+  `description` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `createdAt` datetime(3) NOT NULL DEFAULT current_timestamp(3),
+  `updatedAt` datetime(3) NOT NULL,
+  `quantity` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `inventory`
+--
+
+INSERT INTO `inventory` (`inventory_id`, `description`, `name`, `createdAt`, `updatedAt`, `quantity`) VALUES
+(1, NULL, 'biogesic', '2023-12-03 17:49:59.349', '2023-12-03 18:48:41.707', 45),
+(2, NULL, 'asdasd', '2023-12-03 17:51:21.255', '2023-12-03 18:48:45.821', 7),
+(3, NULL, 'asdad', '2023-12-03 17:51:57.586', '2023-12-03 18:48:49.634', 50),
+(4, NULL, 'bioflue', '2023-12-03 19:49:06.752', '2023-12-03 19:49:26.842', 5);
 
 -- --------------------------------------------------------
 
@@ -115,30 +147,30 @@ CREATE TABLE `event` (
 
 CREATE TABLE `patient` (
   `patient_id` int(11) NOT NULL,
-  `username` varchar(255) NOT NULL,
-  `password` varchar(255) NOT NULL,
-  `first_name` varchar(255) NOT NULL,
-  `middle_name` varchar(255) DEFAULT NULL,
-  `last_name` varchar(255) NOT NULL,
-  `suffix` varchar(255) NOT NULL,
+  `username` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `first_name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `middle_name` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `last_name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `suffix` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `birthdate` date NOT NULL,
-  `contact_number` varchar(255) NOT NULL,
-  `email` varchar(255) NOT NULL,
-  `barangay` varchar(255) NOT NULL,
-  `status` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `contact_number` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `barangay` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `status` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'ACTIVE',
+  `city` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `house_address` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `zip` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `gender` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Dumping data for table `patient`
 --
 
-INSERT INTO `patient` (`patient_id`, `username`, `password`, `first_name`, `middle_name`, `last_name`, `suffix`, `birthdate`, `contact_number`, `email`, `barangay`, `status`) VALUES
-(2, 'test123', '123', 'patient', 'mid', 'last', '', '2006-01-30', '09987654321', 'nalazingapan@gmail.com', '175', 'ACTIVE'),
-(4, 'user', '123', 'user', 'qweq', 'wewqe', '', '2023-11-29', '09053279299', 'nalazingapan@gmail.com', '178 A', 'ACTIVE'),
-(5, 'user123', '123', 'user123', 'user123', 'user123', '', '2023-11-24', '09053279299', 'nalazingapan@gmail.com', '178 A', 'ACTIVE'),
-(6, 'ewe', 'ewe', 'e', 'wew', 'e', 'wew', '2023-11-22', '3123123', 'admin@gmail.com', '174', 'ACTIVE'),
-(7, 'fasdfsdf', 'dwerwe', 'werwe', 'test', 'werwer', '', '2023-11-29', '214312', '2tw@mw', '175', 'ACTIVE'),
-(8, 'dasdas', 'dasda', 'asdas', 'ewe', 'asdas', 'jr', '2023-11-30', '09659312003', 'carlo@gmail.com', '178 A', 'ACTIVE');
+INSERT INTO `patient` (`patient_id`, `username`, `password`, `first_name`, `middle_name`, `last_name`, `suffix`, `birthdate`, `contact_number`, `email`, `barangay`, `status`, `city`, `house_address`, `zip`, `gender`) VALUES
+(8, 'patient1', '$2b$10$Dwl3PWcxYWupB/uNfzJJ5.EftOx6nAlkK4vqjQDGDMjngmBxdxC8q', 'sample', 'sample', 'sample', 'jr', '2023-12-05', '09561289642', 'sample1@gmail.com', '174', 'ACTIVE', 'Hagonoy', 'sample', '3002', 'male'),
+(9, 'patient2', '$2b$10$Dwl3PWcxYWupB/uNfzJJ5.EftOx6nAlkK4vqjQDGDMjngmBxdxC8q', 'sample', 'sample', 'sample', 'jr', '2023-12-05', '09561289642', 'sample2@gmail.com', '174', 'ACTIVE', 'Hagonoy', 'sample', '3002', 'male');
 
 --
 -- Indexes for dumped tables
@@ -154,7 +186,9 @@ ALTER TABLE `admin`
 -- Indexes for table `appointment`
 --
 ALTER TABLE `appointment`
-  ADD PRIMARY KEY (`appointment_id`);
+  ADD PRIMARY KEY (`appointment_id`),
+  ADD KEY `appointment_doctor_id_fkey` (`doctor_id`),
+  ADD KEY `appointment_patient_id_fkey` (`patient_id`);
 
 --
 -- Indexes for table `doctor`
@@ -167,6 +201,12 @@ ALTER TABLE `doctor`
 --
 ALTER TABLE `event`
   ADD PRIMARY KEY (`event_id`);
+
+--
+-- Indexes for table `inventory`
+--
+ALTER TABLE `inventory`
+  ADD PRIMARY KEY (`inventory_id`);
 
 --
 -- Indexes for table `patient`
@@ -182,31 +222,48 @@ ALTER TABLE `patient`
 -- AUTO_INCREMENT for table `admin`
 --
 ALTER TABLE `admin`
-  MODIFY `admin_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=51;
+  MODIFY `admin_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `appointment`
 --
 ALTER TABLE `appointment`
-  MODIFY `appointment_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `appointment_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT for table `doctor`
 --
 ALTER TABLE `doctor`
-  MODIFY `doctor_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `doctor_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `event`
 --
 ALTER TABLE `event`
-  MODIFY `event_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `event_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `inventory`
+--
+ALTER TABLE `inventory`
+  MODIFY `inventory_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `patient`
 --
 ALTER TABLE `patient`
-  MODIFY `patient_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `patient_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `appointment`
+--
+ALTER TABLE `appointment`
+  ADD CONSTRAINT `appointment_doctor_id_fkey` FOREIGN KEY (`doctor_id`) REFERENCES `doctor` (`doctor_id`) ON DELETE SET NULL ON UPDATE CASCADE,
+  ADD CONSTRAINT `appointment_patient_id_fkey` FOREIGN KEY (`patient_id`) REFERENCES `patient` (`patient_id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
